b550ffb887c2b65b2a964dd8931fc7aa185a1184
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-28 09:21:04 -0700

    program that factorize many numbers as poosible into product of two smaller number

---
+++ factor.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <gmp.h>
+
+/**
+ * main - function that factorize as many as possible
+ * into product of two smaller number
+ * @argc: length of args
+ * @argv: array of args in function
+ * Return: 0 if success to end
+ */
+
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	ssize_t line_num;
+	size_t line_n = 0;
+	char *line = NULL;
+	unsigned int num = 2, res;
+	mpz_t n, div_n, mod_n;
+	(void) argc;
+
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Can't open %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while ((line_num = getline(&line, &line_n, file)) != -1)
+	{
+		mpz_init(n);
+		mpz_init(div_n);
+		mpz_init(mod_n);
+		mpz_set_str(n, line, 10);
+		while (num)
+		{
+			if ((res = mpz_mod_ui(mod_n, n, num)) == 0)
+			{
+				mpz_divexact_ui(div_n, n, num);
+				break;
+			}
+			num++;
+		}
+		gmp_printf("%Zd=%Zd*%d\n", n, div_n, num);
+		mpz_clear(n);
+		mpz_clear(div_n);
+		mpz_clear(mod_n);
+		num = 2;
+	}
+	fclose(file);
+	free(line);
+	return (0);
+}
