de9ff05238237b431e96162cb4b5d91aceda3615
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-29 11:32:35 -0700

    change some algorithm to enhance speed

--- factor.c
+++ factor.c
@@ -18,9 +18,9 @@ int main(int argc, char *argv[])
 	ssize_t line_num;
 	size_t line_n = 0;
 	char *line = NULL;
-	mpz_t num, num_half, num_mod, num_div, num_ui, num_mod_ui, num_div_ui;
+	mpz_t num, num_ui, num_mod_ui, num_div_ui;
 	mpz_t num_sqrt, num_mod_sq, num_div_sq;
-	
+
 	if (argc != 2)
 		exit(EXIT_FAILURE);
 
@@ -31,9 +31,6 @@ int main(int argc, char *argv[])
 	while ((line_num = getline(&line, &line_n, file)) != -1)
 	{
 		mpz_init(num);
-		mpz_init(num_half);
-		mpz_init(num_mod);
-		mpz_init(num_div);
 		mpz_init(num_ui);
 		mpz_init(num_mod_ui);
 		mpz_init(num_div_ui);
@@ -43,20 +40,10 @@ int main(int argc, char *argv[])
 
 		mpz_set_str(num, line, 10);
 		mpz_set_ui(num_ui, 2);
-		mpz_divexact_ui(num_half, num, 2);
 		mpz_sqrt(num_sqrt, num);
 
 		while (1)
 		{
-			mpz_mod(num_mod, num, num_half);
-			if (mpz_cmp_ui(num_mod, 0) == 0)
-			{
-				mpz_div(num_div, num, num_half);
-				gmp_printf("%Zd=%Zd*%Zd\n", num, num_half, num_div);
-				break;
-			}
-			mpz_sub_ui(num_half, num_half, 1);
-
 			mpz_mod(num_mod_ui, num, num_ui);
 			if (mpz_cmp_ui(num_mod_ui, 0) == 0)
 			{
@@ -70,15 +57,12 @@ int main(int argc, char *argv[])
 			if (mpz_cmp_ui(num_mod_sq, 0) == 0)
 			{
 				mpz_div(num_div_sq, num, num_sqrt);
-				gmp_printf("%Zd=%Zd*%Zd\n", num, num_sqrt, num_div_sq);
+				gmp_printf("%Zd=%Zd*%Zd\n", num, num_div_sq, num_sqrt);
 				break;
 			}
 			mpz_sub_ui(num_sqrt, num_sqrt, 1);
 		}
 		mpz_clear(num);
-		mpz_clear(num_half);
-		mpz_clear(num_mod);
-		mpz_clear(num_div);
 		mpz_clear(num_ui);
 		mpz_clear(num_mod_ui);
 		mpz_clear(num_div_ui);
