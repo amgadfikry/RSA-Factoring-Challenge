6e5bee95f857043107d2ed45510b9458f1a63bf9
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-29 09:24:50 -0700

    change some algorithm to enhance speed

--- factor.c
+++ factor.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	size_t line_n = 0;
 	char *line = NULL;
 	unsigned int num = 2, res;
-	mpz_t n, div_n, mod_n;
+	mpz_t n, div_n, mod_n, sqrt_n;
 	(void) argc;
 
 	file = fopen(argv[1], "r");
@@ -33,20 +33,29 @@ int main(int argc, char *argv[])
 		mpz_init(n);
 		mpz_init(div_n);
 		mpz_init(mod_n);
+		mpz_init(sqrt_n);
 		mpz_set_str(n, line, 10);
-		while (num)
+		mpz_sqrt(sqrt_n, n);
+		mpz_mod(mod_n, n, sqrt_n);
+		if (mpz_cmp_ui(mod_n, 0) == 0)
+		{
+			gmp_printf("%Zd=%Zd*%Zd\n", n, sqrt_n, sqrt_n);		
+			num = 0;
+		}
+		while (num != 0)
 		{
 			if ((res = mpz_mod_ui(mod_n, n, num)) == 0)
 			{
 				mpz_divexact_ui(div_n, n, num);
+				gmp_printf("%Zd=%Zd*%d\n", n, div_n, num);
 				break;
 			}
 			num++;
 		}
-		gmp_printf("%Zd=%Zd*%d\n", n, div_n, num);
 		mpz_clear(n);
 		mpz_clear(div_n);
 		mpz_clear(mod_n);
+		mpz_clear(sqrt_n);
 		num = 2;
 	}
 	fclose(file);
